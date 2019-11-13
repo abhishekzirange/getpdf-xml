5637e07a01d87484391cba25128fb19548982764
unknown <abhishekzirange@gmail.com>
2019-11-13 12:13:07 +0530

    list_changes

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from pypdf2xmlfolder import pdf2xml
 import re
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import XML, fromstring, tostring
-def get_xmldata(pdf_path):#,addlist,col_headerlist):
+def get_xmldata(pdf_path):
     xml_data = pdf2xml(pdf_path, 'rb')
     xml_data=fromstring(xml_data)
     tree = ET.ElementTree(xml_data)
     root = tree.getroot()
-    adds={}
-    adds1={}
+    xml_data_list=[]
     col_data={}
     for ele in root:
         temp_col1=[]
         for sub_ele in ele:
             print(sub_ele.text)
+            xml_data_list.append(sub_ele.text)
+    return xml_data_list
 
     
 
-get_xmldata('C:/Users/Techforce/Downloads/01.Apr-18_GSTR3B.pdf')
+list_of_xmldata=get_xmldata('C:/Users/Techforce/Downloads/01.Apr-18_GSTR3B.pdf')
